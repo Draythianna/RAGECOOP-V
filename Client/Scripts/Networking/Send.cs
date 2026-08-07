f24c20c02cccfd41f1882c2f971e421cf5bddf47
Draythianna <drathian777@yahoo.com>
2026-08-07 03:59:39 -0400

    spamfix again

--- Client/Scripts/Networking/Send.cs
+++ Client/Scripts/Networking/Send.cs
@@ -10,6 +10,7 @@ namespace RageCoop.Client
 {
     internal static partial class Networking
     {
+        static bool _invalidVehicleLogged = false;
         public static int SyncInterval = 30;
         public static List<NetConnection> Targets = new List<NetConnection>();
 
@@ -47,7 +48,11 @@ namespace RageCoop.Client
                     var veh = ped.CurrentVehicle?.GetSyncEntity() ??
                               ped.VehicleTryingToEnter?.GetSyncEntity() ?? ped.LastVehicle?.GetSyncEntity();
                     p.VehicleID = veh?.ID ?? 0;
-                    if (p.VehicleID == 0) { Log.Debug("Invalid vehicle"); return; }
+                    if (p.VehicleID == 0)
+                    {
+                        if (!_invalidVehicleLogged) { Log.Error("Invalid vehicle"); _invalidVehicleLogged = true; }
+                        return;
+                    }
                     if (p.Speed == 5)
                         p.Seat = ped.GetSeatTryingToEnter();
                     else
