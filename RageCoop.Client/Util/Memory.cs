3dfbba44ddf77fba5f7d6a3ec64843816b754093
Draythianna <drathian777@yahoo.com>
2026-08-28 00:42:44 -0400

    hash renames, wheel slo-mo patch removed, implemented toeulerdegrees inline instead of a class in Quaternion

--- RageCoop.Client/Util/Memory.cs
+++ RageCoop.Client/Util/Memory.cs
@@ -1,81 +1,59 @@
-
 using GTA;
 using GTA.Math;
-using RageCoop.Core;
 using SHVDN;
 using System;
 using System.Collections.Generic;
-using System.Runtime.InteropServices;
 
 namespace RageCoop.Client
 {
-    internal unsafe class MemPatch
-    {
-        private readonly byte[] _data;
-        private readonly byte[] _orginal;
-        private readonly IntPtr _address;
-        public MemPatch(byte* address, byte[] data)
-        {
-            _data = data;
-            _orginal = new byte[data.Length];
-            _address = (IntPtr)address;
-            Marshal.Copy((IntPtr)address, _orginal, 0, data.Length);
-        }
-        public void Install()
-        {
-            Marshal.Copy(_data, 0, _address, _data.Length);
-        }
-        public void Uninstall()
-        {
-            Marshal.Copy(_orginal, 0, _address, _orginal.Length);
-        }
-    }
-
     internal static unsafe class Memory
     {
-        public static MemPatch VignettingPatch;
-        public static MemPatch VignettingCallPatch;
-        public static MemPatch TimeScalePatch;
-        static Memory()
-        {
-            // Weapon/radio wheel slow-mo patch
-            // Thanks @CamxxCore, https://github.com/CamxxCore/GTAVWeaponWheelMod
-            var result = MemScanner.FindPatternBmh("\x38\x51\x64\x74\x19", "xxxxx");
-            if (result == null) { throw new NotSupportedException("Can't find memory pattern to patch weapon/radio slow-mo"); }
-            var address = result + 26;
-            address = address + *(int*)address + 4u;
-            VignettingPatch = new MemPatch(address, new byte[] { RET, 0x90, 0x90, 0x90, 0x90 });
-            VignettingCallPatch = new MemPatch(result + 8, new byte[] { 0x90, 0x90, 0x90, 0x90, 0x90 });
-            TimeScalePatch = new MemPatch(result + 34, new byte[] { XOR_32_64, 0xD2 });
-
-        }
-        public static void ApplyPatches()
-        {
-            VignettingPatch.Install();
-            VignettingCallPatch.Install();
-            TimeScalePatch.Install();
-        }
-        public static void RestorePatches()
-        {
-            VignettingPatch.Uninstall();
-            VignettingCallPatch.Uninstall();
-            TimeScalePatch.Uninstall();
-        }
-        #region PATCHES
-        #endregion
         #region OFFSET-CONST
         public const int PositionOffset = 144;
         public const int VelocityOffset = 800;
         public const int MatrixOffset = 96;
         #endregion
-        #region OPCODE
-        private const byte XOR_32_64 = 0x31;
-        private const byte RET = 0xC3;
-        #endregion
+
+        public static void ApplyPatches()
+        {
+            // Selection wheel slow-mo patch handled by SHVDN Enhanced internally
+            NativeMemory.InstallSelectionWheelsPatch();
+        }
+        public static void RestorePatches()
+        {
+            NativeMemory.UninstallSelectionWheelsPatch();
+        }
+
         public static Vector3 ReadPosition(this Entity e) => ReadVector3(e.MemoryAddress + PositionOffset);
         public static Quaternion ReadQuaternion(this Entity e) => Quaternion.RotationMatrix(e.Matrix);
-        public static Vector3 ReadRotation(this Entity e) => e.ReadQuaternion().ToEulerDegrees();
+        public static Vector3 ReadRotation(this Entity e) => ToEulerDegrees(e.ReadQuaternion());
         public static Vector3 ReadVelocity(this Ped e) => ReadVector3(e.MemoryAddress + VelocityOffset);
+
+        public static Vector3 ToEulerDegrees(Quaternion q)
+        {
+            // Roll (X)
+            float sinrCosp = 2f * (q.W * q.X + q.Y * q.Z);
+            float cosrCosp = 1f - 2f * (q.X * q.X + q.Y * q.Y);
+            float roll = (float)Math.Atan2(sinrCosp, cosrCosp);
+
+            // Pitch (Y)
+            float sinp = 2f * (q.W * q.Y - q.Z * q.X);
+            float pitch = Math.Abs(sinp) >= 1f
+                ? (sinp < 0f ? -1f : 1f) * ((float)Math.PI / 2f)
+                : (float)Math.Asin(sinp);
+
+            // Yaw (Z)
+            float sinyCosp = 2f * (q.W * q.Z + q.X * q.Y);
+            float cosyCosp = 1f - 2f * (q.Y * q.Y + q.Z * q.Z);
+            float yaw = (float)Math.Atan2(sinyCosp, cosyCosp);
+
+            return new Vector3(
+                roll  * (180f / (float)Math.PI),
+                pitch * (180f / (float)Math.PI),
+                yaw   * (180f / (float)Math.PI)
+            );
+        }
+
         public static Vector3 ReadVector3(IntPtr address)
         {
             float* ptr = (float*)address.ToPointer();
