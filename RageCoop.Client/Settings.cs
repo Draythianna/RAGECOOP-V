183173dfda129f6686f25f818c1ff5b81208fb2c
Draythianna <drathian777@yahoo.com>
2026-09-03 19:56:34 -0400

    revert master to official

--- RageCoop.Client/Settings.cs
+++ RageCoop.Client/Settings.cs
@@ -20,7 +20,7 @@ namespace RageCoop.Client
         /// <summary>
         /// Don't use it!
         /// </summary>
-        public string MasterServer { get; set; } = "[AUTO]";
+        public string MasterServer { get; set; } = "https://masterserver.ragecoop.com/";
         /// <summary>
         /// Don't use it!
         /// </summary>
